442d645ae9fe1270262886082b65dd06f318c4b9
MafKamRAN <143469368+MafKamRAN@users.noreply.github.com>
2024-07-10 04:56:37 -0700

    Add files via upload
    This Project is about, when the user clicks on any of the characters it will result in jumping of the character by 360 degrees it is an animation.

---
+++ script.js
@@ -0,0 +1,6 @@
+const letters = document.querySelectorAll(".letters span");
+letters.forEach((letter) => {
+  letter.addEventListener("click", (e) => {
+    e.target.classList.add("active");
+  });
+});
